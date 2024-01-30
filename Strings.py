79783ca3d91311cc426a105dffe72a85db889194
Vlad-Jan <158034547+Vlad-Jan@users.noreply.github.com>
2024-01-30 14:43:37 +0600

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,39 @@
+# Exercise 1
+#x = "Hello World"
+#print(len(x))
+
+# Exercise 2
+#txt = "Hello World"
+#x = txt[0]
+#print(x)
+
+# Exercise 3
+#txt = "Hello World"
+#x = txt[2:5]
+#print(x)
+
+# Exercise 4
+#txt = " Hello World "
+#x = txt.strip()
+#print(txt)
+
+# Exercise 5
+#txt = "Hello World"
+#txt = txt.upper()
+#print(txt)
+
+# Exercise 6
+#txt = "Hello World"
+#txt = txt.lower()
+#print(txt)
+
+# Exercise 7
+#txt = "Hello World"
+#txt = txt.replace("H","J")
+#print(txt)
+
+#Exercise 8
+
+#age = 36
+#txt = "My name is John, and I am {}"
+#print(txt.format(age))
